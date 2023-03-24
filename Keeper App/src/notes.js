b2fd47ff988704e93e96724a3d2a02569d5d0319
Elena Jou <ejouluo@hotmail.com>
2023-03-23 19:02:17 -0700

    Created Keeper app

---
+++ Keeper App/src/notes.js
@@ -0,0 +1,19 @@
+const notes = [
+  {
+    key: 1,
+    title: "First Note",
+    content: "Content of the first note"
+  },
+  {
+    key: 2,
+    title: "Second Note",
+    content: "Content of the first note"
+  },
+  {
+    key: 3,
+    title: "Third Note",
+    content: "Content of the first note"
+  }
+];
+
+export default notes;
